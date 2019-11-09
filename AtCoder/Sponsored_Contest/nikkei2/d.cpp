048ac5898fa7ef9de63dd84e768e62bff163e7a3
tsukasa sawaura <>
2019-11-10 02:03:39 +0900

    nikkei2

---
+++ AtCoder/Sponsored_Contest/nikkei2/d.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll=long long;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<double>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pll=pair<ll,ll>;
+using tll=tuple<ll,ll>;
+using tlll=tuple<ll,ll,ll>;
+using vs=vector<string>;
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define rep(i,n) range(i,0,n)
+#define rrep(i,n) for(ll i=(n)-1;i>=0;i--)
+#define range(i,a,n) for(ll i=(a);i<(n);i++)
+#define LINF ((ll)1ll<<60)
+#define INF ((int)1<<30)
+#define EPS (1e-9)
+#define MOD (1000000007ll)
+#define fcout(a) cout<<setprecision(a)<<fixed
+#define fs first
+#define sc second
+#define PI (3.1415926535897932384)
+
+int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
+template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
+template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+template<class S>S sum(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
+int sgn(const double&r){return (r>EPS)-(r<-EPS);} // a>0  : sgn(a)>0
+int sgn(const double&a,const double&b){return sgn(a-b);} // b<=c : sgn(b,c)<=0
+
+template<class T>void puta(T&&t){cout<<t<<"\n";}
+template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
+template<class S,class T>void tf(bool b,S t,T f){if(b)puta(t);else puta(f);}
+void YN(bool b){tf(b,"YES","NO");}
+void Yn(bool b){tf(b,"Yes","No");}
+void yn(bool b){tf(b,"yes","no");}
+template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;};
+template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1;for(auto s:t){os<<(a?"":" ")<<s;a=0;}return os;}
+template<class S>auto&operator>>(istream&is,vector<S>&t){for(S&a:t)cin>>a;return is;}
+
+/*他のライブラリを入れる場所*/
+struct edge{
+	ll s,t,c;
+	edge(){}
+	edge(ll S,ll T,ll C):s(S),t(T),c(C){}
+};
+
+int main(){
+	cin.tie(0);
+	ios::sync_with_stdio(false);
+	ll n,m;
+	cin>>n>>m;
+	vl dp(n+1,LINF);
+	dp[0]=dp[1]=0;
+	vector<edge> e(m);
+	rep(i,m){
+		ll s,t,c;
+		cin>>s>>t>>c;
+		e[i]=edge(s,t,c);
+	}
+	sort(all(e),[](edge a,edge b){return a.s<b.s or (a.s==b.s and a.c>b.c);});
+	// rep(i,m)puta(e[i].s, e[i].t, e[i].c);
+
+
+	for(auto&u:e){
+		ll cur=dp[u.s];
+		ll nxt=cur+u.c;
+		// 0-t までで、
+		ll p = upper_bound(all(dp),nxt)-dp.begin();
+		range(i,p,u.t+1)dp[i]=nxt;
+		// puta(p,u.t,":",dp);
+	}
+	if(dp[n]==LINF)dp[n]=-1;
+	puta(dp[n]);
+	return 0;
+}
