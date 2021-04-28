fa7238c50dc64708794e183df76bcdd7eaa224cc
pulcherriman <tsukasawa_agu@yahoo.co.jp>
2021-04-28 19:58:26 +0900

    素数

--- lib/LibraryChecker/Factorize.cpp
+++ lib/LibraryChecker/Factorize.cpp
@@ -1,6 +1,27 @@
-#include <bits/stdc++.h>
+#pragma region Perfect Template
+#pragma region Unsecured Optimization
+// #pragma GCC target("avx")
+// #pragma GCC optimize("O3,inline,omit-frame-pointer,no-asynchronous-unwind-tables,fast-math")
+// #pragma GCC optimize("unroll-loops")
+#pragma endregion
+
+#pragma region Include Headers
+#if defined(ONLINE_JUDGE) || defined(_DEBUG)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
 using namespace std;
+
+void Main();
+int main(){cin.tie(nullptr);ios::sync_with_stdio(false);Main();return 0;}
+#pragma endregion
+
+#pragma region Additional Type Definition
 using ll=long long;
+using ld=long double;
 using ull=unsigned long long;
 using vb=vector<bool>;
 using vvb=vector<vb>;
@@ -14,135 +35,218 @@ using pll=pair<ll,ll>;
 using tll=tuple<ll,ll>;
 using tlll=tuple<ll,ll,ll>;
 using vs=vector<string>;
+template<class K> using IndexedSet=__gnu_pbds::tree<K,__gnu_pbds::null_type,less<K>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update>;
+template<class K> using HashSet=__gnu_pbds::gp_hash_table<K,__gnu_pbds::null_type>;
+template<class K,class V> using IndexedMap=__gnu_pbds::tree<K,V,less<K>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update>;
+template<class K,class V> using HashMap=__gnu_pbds::gp_hash_table<K,V>;
+#pragma endregion
+
+#pragma region Macros
 #define all(a) a.begin(),a.end()
 #define rall(a) a.rbegin(),a.rend()
+#define loop(q) __loop(q, __LINE__)
+#define __loop(q,l) __loop2(q,l)
+#define __loop2(q,l) rep(_lp ## l,q)
 #define rep(i,n) range(i,0,n)
-#define rrep(i,n) for(ll i=((ll)n)-1;i>=0;i--)
-#define range(i,a,n) for(ll i=((ll)a);i<((ll)n);i++)
-#define LINF ((ll)1ll<<60)
-#define INF ((int)1<<30)
-#define EPS (1e-9)
-#define MOD (1000000007ll)
+#define rrep(i,n) rrange(i,0,n)
+#define range(i,a,n) for(ll i=((ll)a);i<((ll)n);++i)
+#define rrange(i,a,n) for(ll i=((ll)n-1);i>=((ll)a);--i)
+#define repsq(i,n) for(ll i=0;i*i<=n;++i)
+#define each(v,a) for(auto v:a)
+#define eachref(v,a) for(auto&v:a)
 #define fcout(a) cout<<setprecision(a)<<fixed
-#define fs first
-#define sc second
-#define PI (3.1415926535897932384)
+#pragma endregion
 
-int dx[]={1,0,-1,0,1,-1,-1,1},dy[]={0,1,0,-1,1,1,-1,-1};
-template<class T>bool chmax(T&a,T b){if(a<b){a=b; return true;}return false;}
-template<class T>bool chmin(T&a,T b){if(a>b){a=b; return true;}return false;}
+#pragma region Constants
+constexpr ll LINF=1ll<<60;
+constexpr int INF=1<<30;
+constexpr double EPS=(1e-9);
+constexpr ll MOD=1000000007ll;
+constexpr long double PI=3.14159265358979323846;
+#pragma endregion
+
+#pragma region Output Assist
+template<class T>struct hasItr{
+	template<class U>static constexpr true_type check(class U::iterator*);
+	template<class U>static constexpr false_type check(...);
+	static constexpr bool v=decltype(check<T>(nullptr))::value;
+};
+template<>struct hasItr<string>{static constexpr bool v=false;};
+
+template<class T>void puta(T&t,false_type,ostream&os,[[maybe_unused]]char el){os<<t;}
+template<class T>void puta(T&t,true_type,ostream&os,char el){
+	constexpr bool h=hasItr<typename T::value_type>::v;
+	bool F=true,I;
+	for(auto&i:t){
+		if(!F)os<<' ';
+		puta(i,bool_constant<h>(),os,el);
+		F=I=h;
+	}
+	if(!I)os<<el;
+}
+template<class T>void puta(const T&t, ostream&os=cout, char el='\n'){
+	puta(t,bool_constant<hasItr<T>::v>(),os,el);
+	if(!hasItr<T>::v)os<<el;
+}
+template<class H,class...T>void puta(const H&h,const T&...t){cout<<h<<' ';puta(t...);}
+template<size_t i,class...T>void puta(tuple<T...>const&t, ostream&os){
+	if constexpr(i==sizeof...(T)-1)puta(get<i>(t),os);
+	else{os<<get<i>(t)<<' ';puta<i+1>(t,os);}
+}
+template<class...T>void puta(tuple<T...>const&t, ostream&os=cout){puta<0>(t,os);}
+template<class S,class T>constexpr ostream&operator<<(ostream&os,pair<S,T>p){
+	os<<'['<<p.first<<", ";
+	if constexpr(hasItr<T>::v)puta(p.second,bool_constant<true>(),os,']');
+	else os<<p.second<<']';
+	return os;
+};
+template<class...T>constexpr ostream&operator<<(ostream&os,tuple<T...>t){puta(t,os); return os;}
+void YN(bool b){puta(b?"YES":"NO");}
+void Yn(bool b){puta(b?"Yes":"No");}
+
+#ifdef _DEBUG
+template<class T>void dump_f(const T&t){puta(t,cerr);}
+template<class H,class...T>void dump_f(const H&h,const T&...t){cerr<<h<<' ';dump_f(t...);}
+template<class...T>void dump_f(tuple<T...>const&t){puta(t,cerr);}
+#define dump(...)cerr<<"  "<<string(#__VA_ARGS__)<<": ["<<to_string(__LINE__)<<":"<<__FUNCTION__<<"]\n    ",dump_f(__VA_ARGS__)
+#else
+#define dump(...)                                                              
+#endif
+#pragma endregion
+
+#pragma region Input Assist
+template<class S>auto&operator>>(istream&is,vector<S>&t){for(S&a:t)cin>>a;return is;}
+template<typename...S>void geta_(S&...s){((cin>>s),...);}
+#define geta(t,...) t __VA_ARGS__;geta_(__VA_ARGS__)
+template<class T,class...Args>auto vec(T x,int arg,Args...args){if constexpr(sizeof...(args)==0)return vector(arg,x);else return vector(arg,vec(x,args...));}
+#define getv(a,...) auto a=vec(__VA_ARGS__);cin>>a
+#pragma endregion
+
+#pragma region Utilities
+template<class T>constexpr bool chmax(T&a,T b){return a<b?a=b,1:0;}
+template<class T>constexpr bool chmin(T&a,T b){return a>b?a=b,1:0;}
 template<class S>S sum(vector<S>&a){return accumulate(all(a),S());}
 template<class S>S max(vector<S>&a){return *max_element(all(a));}
 template<class S>S min(vector<S>&a){return *min_element(all(a));}
-ll max(int a,ll b){return max((ll)a,b);} ll max(ll a,int b){return max(a,(ll)b);}
-int sgn(const double&r){return (r>EPS)-(r<-EPS);} // a>0  : sgn(a)>0
-int sgn(const double&a,const double&b){return sgn(a-b);} // b<=c : sgn(b,c)<=0
+template<class T> pair<int,T> getMaxAndIndex(vector<T> a){
+	int p=-1; T v=numeric_limits<T>::min();
+	rep(i,a.size())if(chmax(v,a[i]))p=i;
+	return {p,v};
+}
+#pragma endregion
+#pragma endregion
 
-template<class T>void puta(T&&t){cout<<t<<"\n";}
-template<class H,class...T>void puta(H&&h,T&&...t){cout<<h<<' ';puta(t...);}
-template<class S,class T>void tf(bool b,S t,T f){if(b)puta(t);else puta(f);}
-void YN(bool b){tf(b,"YES","NO");}
-void Yn(bool b){tf(b,"Yes","No");}
-void yn(bool b){tf(b,"yes","no");}
-template<class S,class T>ostream&operator<<(ostream&os,pair<S,T>p){os<<"["<<p.first<<", "<<p.second<<"]";return os;};
-template<class S>auto&operator<<(ostream&os,vector<S>t){bool a=1;for(auto s:t){os<<(a?"":" ")<<s;a=0;}return os;}
-template<class S>auto&operator>>(istream&is,vector<S>&t){for(S&a:t)cin>>a;return is;}
+// ここにライブラリを貼る
+// regionのfoldは[Ctrl+K] => [Ctrl+8] expandは9
+#pragma region Additional Libraries
+
+class Random{
+	unsigned y;
+	constexpr unsigned next(){return y^=(y^=(y^=y<<13)>>17)<<5;}
+public:
+	constexpr Random(const bool&isDeterministic):y(isDeterministic?2463534242:chrono::system_clock::now().time_since_epoch().count()){}
+	constexpr int operator()(int a,int b){return next()%(b-a)+a;}
+	constexpr ll operator()(ll a,ll b){return (((ull)next())<<32|next())%(b-a)+a;}
+	constexpr double operator()(double a,double b){return (b-a)*next()/4294967296.0+a;}
+} Random(0);
+
+struct _Prime {
+private:
+	constexpr ull fast_gcd(ull a, ull b) {
+		if(a==0||b==0)return a+b;
+		int n=__builtin_ctzll(a),m=__builtin_ctzll(b);
+		a>>=n; b>>=m;
+		while(a!=b){ int x=__builtin_ctzll(a-b); a=(max(a,b)-(b=min(a,b)))>>x; }
+		return a<<min(n,m);
+	}
+	constexpr ull mul (ull a,ull b,ull&mod) {
+		ll ret=a*b-mod*(ull)(1.L/mod*a*b);
+		return ret+(ret<0)*mod-(ret>=(ll)mod)*mod;
+	}
+	constexpr ull mod128 (ull a,ull e,ull&mod) {
+		ull ret=1;
+		while(e){ if(e&1)ret=mul(ret,a,mod); a=mul(a,a,mod),e>>=1; }
+		return ret;
+	}
+	constexpr inline ull pollard_f(ull x, ull&n, ull&c){ return mul(x,x,n)+c; }
+	ull pollard(ull n){
+		ull t=0,prod=2,q,x=0,y=0,c=0,i=2;
+		while((t++&127) or fast_gcd(prod,n)==1){
+			if(x==y)y=pollard_f(x=i,n,c=Random(1ll,n));
+			if((q=mul(prod,max(x,y)-min(x,y),n)))prod=q;
+			x=pollard_f(x,n,c),y=pollard_f(pollard_f(y,n,c),n,c);
+		}
+		return fast_gcd(prod,n);
+	}
+public:
+	constexpr bool isPrime (ull n) {
+		if(n<=1)return false;
+		if(n<4)return true;
+		if(n%6%4!=1)return false;
+		ull s=__builtin_ctzll(n-1),d=n>>s;
+
+		constexpr ull pollard_seed[7]={2, 325, 9375, 28178, 450775, 9780504, 1795265022};
+		for(ull x:pollard_seed){
+			ull p=mod128(x,d,n),i=s;
+			while(p!=1 and p!=n-1 and x%n and i--)p=mul(p,p,n);
+			if(p!=n-1 and i!=s) return false;
+		}
+		return true;
+	}
+
+	vector<ull> factor(ull n){
+		if (n==1) return{};
+		if (isPrime(n)) return{n};
+		ull x=pollard(n);
+		auto l=factor(x),r=factor(n/x);
+		l.insert(l.end(),all(r));
+		return l;
+	}
+
+	int divisorCount(ull n){
+		int ret=1; ull v=0,c=0;
+		auto fac=factor(n);
+		sort(all(fac)); fac.emplace_back(-1);
+		for(auto&f:fac){if(f!=v)ret*=c+1, v=f, c=0; c++;}
+		return ret;
+	}
+}; _Prime Prime;
+
+
+class Timer {
+	#ifdef _DEBUG
+	static constexpr uint64_t ClocksPerMsec = 3587000;
+	#else
+	static constexpr uint64_t ClocksPerMsec = 2987000;
+	#endif
+	const uint64_t start,limit;
+	uint64_t getClocks() const{
+		unsigned int lo,hi;
+		__asm__ volatile ("rdtsc" : "=a" (lo), "=d" (hi));
+		return((uint64_t)hi<<32)|lo;
+	}
+public:
+	Timer(uint64_t _limit): start(getClocks()),limit(start+_limit*ClocksPerMsec) {}
+	uint64_t get() const{return (getClocks()-start)/ClocksPerMsec;}
+	operator bool()const{return getClocks()<limit;}
+};
+void wait(const int&msec){Timer tm(msec); while(tm);}
+
+
+#pragma endregion
+
 
 /*他のライブラリを入れる場所*/
-struct _Prime{
-    bool IsPrime(ll n){
-        if(n<2)return false;
-        if(~n&1)return n==2;
-        if(n<=INT_MAX)return IsPrime<int,ll>(n);
-        return IsPrime<ll,__int128_t>(n);
-    }
-    map<ll,int> Factorize(ll n){
-        map<ll,int> r;
-        if(n!=1){
-            if(ll x=pollard_single(n);x==n)r[x]=1;
-            else{
-                r=Factorize(x);
-                for(const auto&v:Factorize(n/x))r[v.first]+=v.second;
-            }
-        }
-        return r;
-    }
-    vl Divisor(ll n){
-        vl r(1,1);
-        for(const auto&[v,c]:Factorize(n))for(int i=0,l=r.size();i<l;++i)rep(j,c)r.push_back(r[i]*pmod(v,j+1,LLONG_MAX));
-        return r;
-    }
 
-    private:
-    template<class T>static constexpr int arr[]={2,7,61};
-    template<class T,class U>
-    static T pmod(T x,U n,T p) {
-        T r=1%p;
-        while(x%=p,n){if(n&1)r=r*x%p;x*=x;n>>=1;}
-        return r;
-    }
-    template<class T,class U>static bool IsPrime(T n){
-        for(const int&a:arr<T>){
-            bool c=true;
-            ll m=n-1;
-            while(~m&1)c&=pmod<U>(a,m>>=1,n)!=n-1;
-            c&=pmod<U>(a,m?:1,n)!=1;
-            if(c)return n<=a;
-        }
-        return true;
-    }
-
-    ll fast_gcd(ll _a, ll _b) {
-        ll a=abs(_a),b=abs(_b);
-        if(a==0||b==0)return a+b;
-        int n=__builtin_ctzll(a),m=__builtin_ctzll(b);
-        a>>=n;b>>=m;
-        while(a!=b){
-            int x=__builtin_ctzll(a-b);
-            ll c=a>b?a:b;
-            a=(c-=b=a>b?b:a)>>x;
-        }
-        return a<<min(n,m);
-    }
-    ll pollard_single(ll n){
-        const static auto f=[&n](ll x){return(__int128_t(x)*x+1)%n;};
-        if(~n&1)return 2;
-        if(IsPrime(n))return n;
-        ll st=0;
-        while(true){
-            ll x=++st,y=f(x);
-            while(true){
-                ll p=fast_gcd(y-x+n,n);
-                if(p==0||p==n)break;
-                if(p!=1)return p;
-                x=f(x);
-                y=f(f(y));
-            }
-        }
-    }
-};
-template<>constexpr int _Prime::arr<ll>[]={2,325,9375,28178,450775,9780504,1795265022};
-_Prime Prime;
-
-
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
-
-    ll n; cin>>n;
-    rep(i,n){
-        ll t; cin>>t;
-        auto f = Prime.Factorize(t);
-        
-        ll c=0;
-        for(auto p:f)c+=p.second;
-
-        vl ans(1,c);
-        for(auto p:f){
-            rep(j,p.second) ans.push_back(p.first);
-        }
-        puta(ans);
-    }
-    return 0;
-
+void Main(){
+	Timer timer(10000);
+	geta(ll,q);
+	loop(q){
+		geta(ll,n);
+		auto facs=Prime.factor(n);
+		sort(all(facs));
+		puta(facs.size(), facs);
+	}
+	dump(timer.get());
+	return;
 }
