a0733c04c2663da660d01bdba9f7a8af64f97f4f
pulcherriman <tsukasawa_agu@yahoo.co.jp>
2021-10-14 20:14:28 +0900

    httf4Y+

---
+++ AtCoder/Others/Future-Fif-Digital-Days-Open/test.bat
@@ -0,0 +1,46 @@
+@echo off
+setlocal enabledelayedexpansion
+prompt $$$S
+if "%1" == "" (
+	echo ファイルを入力してください
+	exit
+)
+
+set generator_path=tester/generator.py
+set judge_path=tester/judge.py
+
+pushd test_env
+
+g++ -std=gnu++17 -Wall -Wno-unknown-pragmas -O2 %1
+echo Compile Finished.
+
+set batchsize=10
+set current=0
+set sum=0
+set max=0
+set min=100000000
+for /l %%c in (1,1,100) do (
+	for /l %%i in (1,1,%batchsize%) do (
+		python %generator_path% !random! > input.txt
+		a.exe < input.txt > output.txt
+		python %judge_path% input.txt output.txt
+		set score=!errorlevel!
+		set /a sum+=!score!
+		if !score! gtr !max! (
+			set max=!score!
+		)
+		if !score! lss !min! (
+			set min=!score!
+		)
+		set /a current+=1
+	)
+	set /a average=sum/!current!
+	set /a expected=!average!/2
+	cls
+	echo   Case #: !current!
+	echo  Average: !average!
+	echo Expected: !expected!00
+	echo      Max: !max!
+	echo      Min: !min!
+)
+popd
