b412806449461e264b38b128ac82e8e2b451d878
pulcherriman <tsukasawa_agu@yahoo.co.jp>
2022-06-22 02:19:41 +0900

    ABC256

---
+++ AtCoder/ABC/ABC256/abc256_g.cpp
@@ -0,0 +1,484 @@
+// @prefix _template
+// @description 新テンプレート
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("inline")
+#pragma GCC diagnostic ignored "-Wunused-value"
+#ifdef _DEBUG
+// #define _GLIBCXX_DEBUG 1
+// #define _GLIBCXX_DEBUG_PEDANTIC 1
+#endif
+
+/* 
+ * Include Headers
+ */
+#if defined(EVAL) || defined(ONLINE_JUDGE) || defined(_DEBUG)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+
+/*
+ * Additional Type Definition
+ */
+using ll=long long;
+using ld=long double;
+using ull=unsigned long long;
+using vb=vector<bool>;
+using vvb=vector<vb>;
+using vd=vector<ld>;
+using vvd=vector<vd>;
+using vi=vector<int>;
+using vvi=vector<vi>;
+using vl=vector<ll>;
+using vvl=vector<vl>;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+using vp=vector<pll>;
+using tl2=tuple<ll,ll>;
+using tl3=tuple<ll,ll,ll>;
+using vs=vector<string>;
+template<class K> using IndexedSet=__gnu_pbds::tree<K,__gnu_pbds::null_type,less<K>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update>;
+template<class K> using HashSet=__gnu_pbds::gp_hash_table<K,__gnu_pbds::null_type>;
+template<class K,class V> using IndexedMap=__gnu_pbds::tree<K,V,less<K>,__gnu_pbds::rb_tree_tag,__gnu_pbds::tree_order_statistics_node_update>;
+template<class K,class V> using HashMap=__gnu_pbds::gp_hash_table<K,V>;
+template<class V> using minpq = priority_queue<V, vector<V>, greater<V>>;
+
+#define all(a) begin(a),end(a)
+#define rall(a) rbegin(a),rend(a)
+#define __LOOPSWITCH(_1, _2, _3, __LOOPSWITCH, ...) __LOOPSWITCH
+#define rep(...) __LOOPSWITCH(__VA_ARGS__, __RANGE, __REP, __LOOP) (__VA_ARGS__)
+#define rrep(...) __LOOPSWITCH(__VA_ARGS__, __RRANGE, __RREP, __LOOP) (__VA_ARGS__)
+#define __LOOP(q) __LOOP2(q, __LINE__)
+#define __LOOP2(q,l) __LOOP3(q,l)
+#define __LOOP3(q,l) __REP(_lp ## l,q)
+#define __REP(i,n) __RANGE(i,0,n)
+#define __RANGE(i,a,n) for(ll i=((ll)a);i<((ll)n);++i)
+#define __RREP(i,n) __RRANGE(i,0,n)
+#define __RRANGE(i,a,n) for(ll i=((ll)(n)-1);i>=((ll)a);--i)
+#define sz(a) ((ll)(a).size())
+
+/*
+ * Constants
+ */
+constexpr ll LINF=1ll<<60;
+constexpr int INF=1<<30;
+constexpr double EPS=(1e-14);
+constexpr ll MOD=998244353ll;
+constexpr long double PI=3.14159265358979323846;
+
+/*
+ * Utilities
+ */
+template<class T>constexpr bool chmax(T&a,T b){return a<b?a=b,1:0;}
+template<class T>constexpr bool chmin(T&a,T b){return a>b?a=b,1:0;}
+template<class S>S sum(vector<S>&a){return accumulate(all(a),S());}
+template<class S>S max(vector<S>&a){return *max_element(all(a));}
+template<class S>S min(vector<S>&a){return *min_element(all(a));}
+
+namespace IO {
+	// container detection
+	template<typename T, typename _=void> struct is_container : false_type {};
+	template<> struct is_container<string> : false_type {};
+	template<typename...Ts> struct is_container_helper {};
+	template<typename T> struct is_container<T, conditional_t<
+		true, void, is_container_helper<
+			typename T::value_type, typename T::size_type, typename T::iterator,
+			decltype(declval<T>().size()),
+			decltype(declval<T>().begin()),
+			decltype(declval<T>().end()) >>> : public true_type {};
+	template<typename T,
+		typename enable_if<is_container<T>{}, nullptr_t>::type = nullptr,
+		char Separator = is_container<typename T::value_type>{} ? '\n' : ' ' >
+	constexpr ostream&operator<<(ostream&os, T t){
+		if(auto b=begin(t), e=end(t) ; !t.empty()) for(os<<(*b++);b!=e;os<<Separator<<(*b++)) ;
+		return os;
+	}
+
+	// output
+	template<class T, class...Ts> constexpr ostream& pargs(ostream&os, T&&t, Ts&&...args); // support clang
+	template<class S,class T>constexpr ostream&operator<<(ostream&os,pair<S,T>p){ return os<<'['<<p.first<<", "<<p.second<<']'; };
+	template<class...Ts>constexpr ostream&operator<<(ostream&os,tuple<Ts...>t){
+		return apply([&os](auto&&t,auto&&...args)->ostream&{return pargs(os, t, args...);}, t);
+	};
+	template<class T, class...Ts> constexpr ostream& pargs(ostream&os, T&&t, Ts&&...args) {
+		return ((os<<t)<<...<<(os<<' ', args));
+	}
+
+	template<class...Ts> constexpr ostream& out(Ts...args) { return pargs(cout, args...)<<'\n'; }
+	template<class...Ts> constexpr ostream& debug_f(Ts...args) { return pargs(cerr, args...)<<'\n'; }
+	#ifdef _DEBUG
+	template<class...Ts> constexpr ostream& debug(Ts...args) { return pargs(cerr, args...)<<'\n'; }
+	#else
+	#define debug(...) if(false)debug_f(__VA_ARGS__)
+	#endif
+	void Yn(bool f) { out(f?"Yes":"No"); }
+
+	// input
+	template<class T, class...Ts> constexpr istream& gargs(istream&is, T&&t, Ts&&...args) {
+		return ((is>>t)>>...>>args);
+	}
+	template<class S,class T>auto&operator>>(istream&is,pair<S,T>&p){return is>>p.first>>p.second;}
+	template<class...Ts>constexpr istream&operator>>(istream&is,tuple<Ts...>&t){
+		return apply([&is](auto&&t,auto&&...args)->istream&{return gargs(is, t, args...);}, t);
+	};
+
+	template<typename...S>auto&in(S&...s){return gargs(cin, s...);}
+	#define def(t,...) t __VA_ARGS__; in(__VA_ARGS__)
+	template<typename T, typename enable_if<is_container<T>{}, nullptr_t>::type = nullptr>
+	auto&operator>>(istream&is,T&t){for(auto&a:t)is>>a; return is;}
+}
+using namespace IO;
+
+class Random {
+public:
+	typedef uint_fast32_t result_type;
+	constexpr result_type operator()(){return operator()((ll)min(),(ll)max());}
+	static constexpr result_type max(){return numeric_limits<result_type>::max();}
+	static constexpr result_type min(){return 0;}
+	constexpr Random(const bool&isDeterministic):y(isDeterministic?2463534242:chrono::system_clock::now().time_since_epoch().count()){}
+	constexpr int operator()(int a,int b){return next()%(b-a)+a;}
+	constexpr ll operator()(ll a,ll b){return (((ull)next())<<32|next())%(b-a)+a;}
+	constexpr double operator()(double a,double b){return (b-a)*next()/4294967296.0+a;}
+private:
+	result_type y;
+	constexpr result_type next(){return y^=(y^=(y^=y<<13)>>17)<<5;}
+} Random(0);
+
+class Timer {
+	#ifdef _DEBUG
+	static constexpr uint64_t ClocksPerMsec = 3587000;
+	#else
+	static constexpr uint64_t ClocksPerMsec = 2987000;
+	#endif
+	const uint64_t start,limit;
+	uint64_t getClocks() const{
+		unsigned int lo,hi;
+		__asm__ volatile ("rdtsc" : "=a" (lo), "=d" (hi));
+		return((uint64_t)hi<<32)|lo;
+	}
+public:
+	Timer(uint64_t _limit=1970): start(getClocks()),limit(start+_limit*ClocksPerMsec) {}
+	uint64_t get() const{return (getClocks()-start)/ClocksPerMsec;}
+	operator bool()const{return getClocks()<limit;}
+};
+void wait(const int&msec){Timer tm(msec); while(tm);}
+
+struct Mgr {
+	static const int TLE = 2000;
+	static inline Timer timer = Timer(TLE-20);
+	Mgr() {
+		ios_base::sync_with_stdio(0); cin.tie(0);
+		cout<<fixed<<setprecision(11);
+		cerr<<fixed<<setprecision(3);
+	}
+	~Mgr(){
+		debug_f(timer.get(), "ms")<<flush;
+	}
+} _manager;
+
+namespace std::tr1 {
+	template<class T>
+	struct hash_base {
+		const static inline size_t hash_value = 0x9e3779b9;
+		static inline size_t hash_rnd = Random(0, numeric_limits<size_t>::max());
+		template<class V> static size_t& do_hash(size_t&seed, V&v) {
+			return seed ^= hash<V>{}(v) + hash_value + (seed<<6) + (seed>2);
+		}
+		virtual size_t operator()(T p) const = 0;
+	};
+
+	template<class S, class T>
+	struct hash<pair<S,T>> : public hash_base<pair<S,T>> {
+		size_t operator()(pair<S,T> p) const {
+			size_t seed = 0;
+			this->do_hash(seed, p.first);
+			this->do_hash(seed, p.second);
+			return this->do_hash(seed, this->hash_value);
+		}
+	};
+}
+
+template <int mod> struct ModInt {
+	// 原始根 O(sqrt(N))
+	static int get_primitive_root() {
+		static int primitive_root = 0;
+		if (!primitive_root) {
+			primitive_root = -1;
+			set<int> fac;
+			int v=mod-1;
+			for(int64_t i=2;i*i<=v;i++)while(v%i)fac.insert(i),v/=i;
+			if(v>1)fac.insert(v);
+			rep(g,1,mod){
+				bool ok=true;
+				for(auto&i:fac) if(ModInt(g).pow((mod-1)/i)==1){ ok=false; break; }
+				if(ok){ primitive_root=g; break; }
+			}
+		}
+		return primitive_root;
+	}
+	int x;
+	ModInt() : x(0) {}
+	ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+	explicit operator bool() const { return x != 0; }
+	explicit operator int64_t() const { return x; }
+	ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+	ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+	ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+	ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+	ModInt operator-() const { return ModInt(-x); }
+	ModInt &operator+=(const ModInt &p) { if((x += p.x) >= mod) x -= mod; return *this; }
+	ModInt &operator-=(const ModInt &p) { if((x += mod - p.x) >= mod) x -= mod; return *this; }
+	ModInt &operator*=(const ModInt &p) { x = (int) (1LL * x * p.x % mod); return *this; }
+	ModInt &operator/=(const ModInt &p) { return *this *= p.inv(); }
+	friend ModInt operator+(int64_t a, const ModInt &p) { return p+a; }
+	friend ModInt operator-(int64_t a, const ModInt &p) { return -p+a; }
+	friend ModInt operator*(int64_t a, const ModInt &p) { return p*a; }
+	friend ModInt operator/(int64_t a, const ModInt &p) { return p.inv()*a; }
+	bool operator==(const ModInt &p) const { return x == p.x; }
+	bool operator!=(const ModInt &p) const { return x != p.x; }
+	bool operator<(const ModInt &p) const { return x < p.x; }
+	friend istream &operator>>(istream &is, ModInt &p) { int64_t t; is >> t; p = ModInt(t); return is; }
+	friend ostream &operator<<(ostream &os, const ModInt &p) { return os << p.x; }
+	ModInt pow(int64_t n) const {
+		ModInt ret(1), mul(x);
+		while(n > 0) {
+			if(n & 1) ret *= mul;
+			mul *= mul;
+			n >>= 1;
+		}
+		return ret;
+	}
+
+	ModInt inv() const {
+		int a = x, b = mod, u = 1, v = 0, t;
+		while(b > 0) {
+			t = a / b;
+			swap(a -= t * b, b);
+			swap(u -= t * v, v);
+		}
+		return u;
+	}
+
+	ModInt sqrt() const {
+		if (x==0) return 0;
+		if (mod==2) return x;
+		if (pow((mod-1)/2)!=1) return 0;
+		ModInt b = 1;
+		while (b.pow((mod-1)/2)==1)b+=1;
+		int e=0, m=mod-1;
+		while(m%2==0)m>>=1, e++;
+		ModInt p=pow((m-1)/2), y=(*this)*p*p, z=b.pow(m);
+		p*=(*this);
+		while(y!=1){
+			int j=0;
+			ModInt t=y;
+			while(t!=1)j++, t*=t;
+			z=z.pow(1LL<<(e-j-1));
+			p*=z,z*=z,y*=z,e=j;
+		}
+		return ModInt(min(p.x, mod-p.x));
+	}
+};
+using mymint = ModInt<MOD>;
+using vm = vector<mymint>;
+using vvm = vector<vm>;
+mymint operator"" _m(unsigned long long a){ return mymint(a); }
+
+template<class T>struct Combination{
+	int size=0;
+	vector<T> fact, rfact, inv;
+	Combination(int sz) : size(sz), fact(sz+1), rfact(sz+1), inv(sz+1) {
+		fact[0] = rfact[sz] = inv[0] = 1;
+		rep(i,1,sz+1) fact[i] = fact[i-1] * i;
+		rfact[sz] /= fact[sz];
+		rrep(i,1,sz+1) rfact[i-1] = rfact[i] * i;
+		rep(i,1,sz+1) inv[i] = rfact[i] * fact[i-1];
+	}
+
+	T P(int n, int r) const {
+		if(r<0 or n<r) return 0;
+		if(n <= size) return fact[n] * rfact[n-r];
+		T ret(1);
+		rep(r) ret*=n--;
+		return ret;
+	}
+
+	T C(int n, int r) const {
+		if(r<0 or n<r) return 0;
+		if(n <= size) return fact[n] * rfact[r] * rfact[n-r];
+		chmin(r,n-r);
+		T ret(1);
+		rep(i,1,r+1){ ret*=n--; ret/=i; }
+		return ret;
+	}
+
+	T H(int n, int r) const {
+		if(n<0 or r<0) return 0;
+		return r==0 ? 1 : C(n+r-1, r);
+	}
+};
+
+Combination<mymint> comb(1000000);
+struct nCr{ nCr(int _) {}
+	mymint operator()(int n, int r)const{ return comb.C(n,r); }
+} nCr(0);
+struct nPr{ nPr(int _) {}
+	mymint operator()(int n, int r)const{ return comb.P(n,r); }
+} nPr(0);
+struct nHr{ nHr(int _) {}
+	mymint operator()(int n, int r)const{ return comb.H(n,r); }
+} nHr(0);
+
+
+
+
+
+
+
+
+template <typename T> struct Matrix {
+	int H, W;
+	vector<T> elem;
+	typename vector<T>::iterator operator[](int i) { return elem.begin() + i * W; }
+	inline T &at(int i, int j) { return elem[i * W + j]; }
+	inline T get(int i, int j) const { return elem[i * W + j]; }
+	operator vector<vector<T>>() const {
+		vector<vector<T>> ret(H);
+		rep(i,H) copy(elem.begin()+i*W, elem.begin()+(i+1)*W, back_inserter(ret[i]));
+		return ret;
+	}
+ 
+	Matrix() = default;
+	Matrix(int N) : Matrix(N, N) {}
+	Matrix(int H, int W) : H(H), W(W), elem(H * W, 0) {}
+	Matrix(const vector<vector<T>> &d) : H(d.size()), W(d.size() ? d[0].size() : 0) {
+		for (auto &raw : d) copy(raw.begin(), raw.end(), back_inserter(elem));
+	}
+ 
+	static Matrix Identity(int N){ Matrix ret(N, N); rep(i,N)ret.at(i,i)=1; return ret; }
+ 
+	Matrix operator-() const{ Matrix ret(H, W); rep(i,H*W)ret.elem[i]=-elem[i]; return ret; }
+	Matrix operator*(const T &v) const { Matrix ret=*this; for(auto&x:ret.elem)x*=v; return ret; }
+	Matrix operator/(const T &v) const { Matrix ret=*this; for(auto&x:ret.elem)x/=v; return ret; }
+	Matrix operator+(const Matrix &r) const { Matrix ret=*this; rep(i,H*W)ret.elem[i]+=r.elem[i]; return ret;}
+	Matrix operator-(const Matrix &r) const { Matrix ret=*this; rep(i,H*W)ret.elem[i]-=r.elem[i]; return ret; }
+	Matrix operator*(const Matrix &r) const { Matrix ret(H, r.W); rep(i,H)rep(k,W)rep(j,r.W)ret.at(i, j)+=this->get(i, k)*r.get(k, j); return ret; }
+	Matrix &operator*=(const T &v) { return *this=*this*v; }
+	Matrix &operator/=(const T &v) { return *this=*this/v; }
+	Matrix &operator+=(const Matrix &r) { return *this=*this+r; }
+	Matrix &operator-=(const Matrix &r) { return *this=*this-r; }
+	Matrix &operator*=(const Matrix &r) { return *this=*this*r; }
+	bool operator==(const Matrix &r) const { return H == r.H && W == r.W && elem == r.elem; }
+	bool operator!=(const Matrix &r) const { return !(*this==r); }
+	bool operator<(const Matrix &r) const { return elem < r.elem; }
+	Matrix pow(int64_t n) const {
+		Matrix ret=Identity(H);
+		if(n)rrep(i,64-__builtin_clzll(n)){ if(ret*=ret;(n>>i)&1)ret*=*this; }
+		return ret;
+	}
+	// 転置
+	Matrix transpose() const {
+		Matrix ret(W, H);
+		rep(i,H)rep(j,W)ret.at(j, i)=this->get(i, j);
+		return ret;
+	}
+	// ガウスの掃き出し法 (整数でもdoubleにすること)
+	// O(H^2 W)
+	Matrix gauss_jordan() const {
+		int c = 0;
+		Matrix mtr(*this);
+		vector<int> ws;
+		ws.reserve(W);
+		rep(h,H) {
+			if (c == W) break;
+			int piv = -1;
+			rep(j,h,H)if(mtr.get(j, c)){piv=j; break;}
+			if(piv==-1){c++; h--; continue;}
+			if (h != piv)rep(w,W){
+				swap(mtr[piv][w], mtr[h][w]);
+				mtr.at(piv, w) *= -1;
+			}
+			ws.clear();
+			rep(w,c,W)if(mtr.at(h, w)) ws.emplace_back(w);
+			rep(hh,H)if(hh!=h){
+				const T coeff=mtr.at(hh, c)/mtr.at(h, c);
+				for(auto&w:ws)mtr.at(hh, w)-=mtr.at(h, w)*coeff;
+			}
+			c++;
+		}
+		return mtr;
+	}
+	// 掃き出し済みを仮定したランク
+	// ランク=変数の個数なら唯一解、ランク<変数の個数なら不定解
+	// 係数行列のランク!=拡大係数行列のランクの場合、この値にかかわらず解なし
+	int rank_of_gauss_jordan() const { rrep(i,H*W)if(elem[i])return i/W+1; return 0; }
+	// 上三角行列（=掃き出し済み）を仮定した行列式
+	T determinant_of_upper_triangle() const { T ret=1; rep(i,H)ret*=get(i, i); return ret; }
+	// 逆行列
+	int inverse() {
+		assert(H == W);
+		vector<vector<T>> ret = Identity(H), tmp = *this;
+		int rank = 0;
+		rep(i,H) {
+			int ti = i;
+			while (ti < H and tmp[ti][i] == 0) ti++;
+			if (ti == H) continue;
+			rank++;
+			ret[i].swap(ret[ti]), tmp[i].swap(tmp[ti]);
+			T inv = tmp[i][i].inv();
+			rep(j,W) ret[i][j] *= inv;
+			rep(j,i+1,W) tmp[i][j] *= inv;
+			rep(h,H)if(i!=h){
+				const T c = -tmp[h][i];
+				rep(j,W) ret[h][j] += ret[i][j] * c;
+				rep(j,i+1,W) tmp[h][j] += tmp[i][j] * c;
+			}
+		}
+		*this = ret;
+		return rank;
+	}
+	friend vector<T> operator*(const Matrix &m, const vector<T> &v) {
+		assert(m.W == int(v.size()));
+		vector<T> ret(m.H);
+		rep(i,m.H)rep(j,m.W) ret[i] += m.get(i, j) * v[j];
+		return ret;
+	}
+	friend vector<T> operator*(const vector<T> &v, const Matrix &m) {
+		assert(int(v.size()) == m.H);
+		vector<T> ret(m.W);
+		rep(i,m.H)rep(j,m.W) ret[j] += v[i] * m.get(i, j);
+		return ret;
+	}
+	friend ostream &operator<<(ostream &os, const Matrix &x) {
+		puta((vector<vector<T>>)x, os); return os;
+	}
+	friend istream &operator>>(istream &is, Matrix &x) {
+		return is>>x.elem;
+	}
+};
+
+int main() {
+	/*$1*/
+	def(ll,n,d);
+	vm w(d,0);
+	rep(i,d)w[i]=nCr(d-1,i);
+
+
+	mymint ans=0;
+	rep(white,d+2){
+		Matrix<mymint> m(2,2);
+		m[0][0] = white<=d-1 ? w[white] : 0;
+		m[0][1] = white>=1 and white-1<=d-1 ? w[white-1] : 0;
+		m[1][0] = white>=1 and white-1<=d-1 ? w[white-1] : 0;
+		m[1][1] = white>=2 and white-2<=d-1 ? w[white-2] : 0;
+		m=m.pow(n);
+
+		ans+=(vm{1,0}*m)[0];
+		ans+=(vm{0,1}*m)[1];
+	}
+	out(ans);
+}
